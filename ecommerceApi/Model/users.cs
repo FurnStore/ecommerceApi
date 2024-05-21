d0b7df482c12150a3856456ade949217f728ab6b
Habiba Elhadary <habibaelhadary991@gmail.com>
2024-05-21 23:20:30 +0300

    Add project files.

---
+++ ecommerceApi/Model/users.cs
@@ -0,0 +1,17 @@
+﻿using ecommerceApi.Helpers;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ecommerceApi.Model
+{
+    public class users:IdentityUser
+    {
+     
+        [StringLength(10, MinimumLength = 3)]
+        public string fullName { get; set; }
+        
+        
+    }
+}
